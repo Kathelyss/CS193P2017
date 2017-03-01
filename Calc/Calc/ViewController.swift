6ab5508828be62f71357acc7644cd2cfb57eb6cd
Kathelyss <rubicante@icloud.com>
2017-03-01 21:28:53 +0300

    add gitignore and project files

---
+++ Calc/Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Екатерина Рыжова on 01.03.17.
+//  Copyright © 2017 Ekaterina Ryzhova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
